9bcfa24128a27a0139c145e01e77a249cab63d7d
Zusza <114587290+Zusza@users.noreply.github.com>
2022-10-25 15:34:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# Zad1
+# print("Wszystkie dni listopada")
+# for i in range(1,31):
+#   print(i)
+
+# Zad2
+# a = int(input("Podaj zakres: "))
+# for i in range(1, a+1):
+#   if i % 2 != 0:
+#     print(i**2)
+
+# Zad3
+# for i in range(1000,10000):
+#   if i%379 == 0:
+#     print(i)
+# Zad4
+# for i in range(100,1000):
+#    if i%5 == 0:
+#      print(i)
+# for i in range(100,1000):
+#    if i%6 == 0:
+#      print(i)
+# for i in range(100,1000):
+#    if i%11 == 0:
+#      print(i)
+
+# Zad5
+# n = int(input("Podaj ile chcesz dodać liczb: "))
+# suma = 0
+# for i in range(n):
+#   a = int(input("Podaj liczbe "))
+#   suma = suma + a
+# print("Suma wynosi: ", suma)
+
+# Zad6
+# k = int(input("Podaj k: "))
+# suma = 0
+# for i in range(k):
+#    suma = k * 2
+# print("Suma wynosi:", suma)
+
+# Zad8
+# W0 = int(input("Podaj W0: "))
+# L = int(input("Podaj L: "))
+# m = L*12
+# w = W0
+# for i in range(1,m) :
+#   w = w + 0.005*w
+# print("Wartość inwestycji wynosi: ", round(w,2))
+
